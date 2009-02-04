94749873dd26f0519319ccef073440093ffe1390
Eduard Bondarenko <edbond@gmail.com>
2009-02-04 16:21:56 +0200

    naive algo (again)

--- 3.hs
+++ 3.hs
@@ -1,39 +1,42 @@
--- naive
--- ./3 600851475  2.48s user 0.04s system 96% cpu 2.605 total
+--- http://projecteuler.net/index.php?section=problems&id=3
+---
+--- The prime factors of 13195 are 5, 7, 13 and 29.
+--- What is the largest prime factor of the number 600851475143 ?
+---
+--- naive:
+--- ./3 600851475  2.48s user 0.04s system 96% cpu 2.605 total
 
 module Main
 where
 
 import System
 
--- решето Эратосфена
+isPrime :: (Integral a) => a -> Bool
+isPrime x = all (\f -> x `rem` f /= 0) [2..x-1]
 
--- удаляет из списка числа, которые делятся на x
-removeMults :: (Integral a) => a -> [a] -> [a]
-removeMults x = filter (\f -> f `rem` x /= 0)
+primesUntil :: (Integral a) => a -> [a]
+primesUntil a = filter isPrime [2..a]
 
-sieve :: (Integral a) => a -> [a] -> [a] -> [a]
-sieve _ x [] = x
-sieve h e (b:bs) | b*b > h = e ++ bs
-sieve h e (b:bs) = let
-  r = removeMults b bs in
-  sieve h (e ++ [b]) r
-
-sieveUntil :: (Integral a) => a -> [a]
-sieveUntil a = sieve a [] [2..a]
-
--- The prime factors of 13195 are 5, 7, 13 and 29.
 primeFactors :: (Integral a) => a -> [a]
-primeFactors x = filter (\f -> x `rem` f == 0) primes
-  where primes = sieveUntil(x)
+primeFactors a =
+  let
+    s = ceiling . sqrt $ fromIntegral a
+    p = primesUntil s
+  in
+    filter (\f -> a `rem` f == 0) p
 
 largestPrimeFactor :: (Integral a) => (a -> a)
-largestPrimeFactor = last . primeFactors
+largestPrimeFactor = head . reverse . primeFactors
 
--- 600851475143
 main = do
   argv <- getArgs
   let
     num = head argv
     result = largestPrimeFactor $ (read num)::Integer in
     putStrLn $ show result
+
+test :: IO ()
+test =
+  case largestPrimeFactor 13195 of
+    29 -> putStrLn "OK"
+    otherwise -> putStrLn "FAIL!"
