c8681af61c10a7f45ae926ecf4e6791d404c33b7
Eduard Bondarenko <edbond@gmail.com>
2009-02-04 22:26:56 +0200

    find run

--- 50.hs
+++ 50.hs
@@ -11,9 +11,18 @@ module Main
 where
 
 isPrime :: (Integral a) => a -> Bool
+isPrime 1 = True
+isPrime 2 = True
 isPrime x = let
-  u = ceiling . sqrt $ fromIntegral x
+  up = ceiling . sqrt $ fromIntegral x
   in
-  all (\f -> x `rem` f /= 0) [2..u]
+  all (\f -> x `rem` f /= 0) [2..up]
+
+primesBelow x = filter isPrime [x,x-1..1]
+
+findRun prime l = let
+  len = length l
+  in
+  map (\n -> sum $ take n l) [1..len]
 
 --main = putStrLn "50"
