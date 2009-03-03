9f2372bc39d6fd9aa0307683471dbc882ad44431
Eduard Bondarenko <edbond@gmail.com>
2009-03-03 17:42:56 +0200

    naive

--- 224_naive.hs
+++ 224_naive.hs
@@ -1,27 +1,38 @@
+-- http://projecteuler.net/index.php?section=problems&id=224
+--
+-- Let us call an integer sided triangle with sides a ≤ b ≤ c barely obtuse if the sides satisfy
+-- a^(2) + b^(2) = c^(2) - 1.
+-- How many barely obtuse triangles are there with perimeter ≤ 75,000,000?
+
+
+{-# OPTIONS -funbox-strict-fields #-}
+
 module Main
 where
 
-import GHC.Base
-import GHC.Float
+import Debug.Trace
+import Control.Parallel.Strategies
 
-lim = 7500
+lim = 75000000
 
 eq a b c = a*a+b*b == c*c-1
-
---perim :: Int# -> Int# -> Int# -> Bool
-perim a b c = a+b+c <= lim
-{-# INLINE perim #-}
+{-# INLINE eq #-}
 
 iterAB a b = let
   l = max a b
-  h = min (a+b) (lim-a-b)
-  cc = any (\c -> (perim a b c) && (eq a b c)) [l..h]
+  ch = truncate $ sqrt $ fromIntegral (a*a+b*b+1)
+  h = minimum [a+b, lim-a-b, ch]
+
+  cc :: Bool
+  cc = any (eq a b) [l..h]
+  --cc = elem True (parMap rnf (eq a b) [l..h])
   in
   cc
+  --traceShow ("a",a, "b",b, "l",l, "h",h, "cc",cc) cc
 
 iterA a = let
   l = a
-  h = lim-a
+  h = (lim-a) `div` 2
   b = any (iterAB a) [l..h]
   in
   case b of
